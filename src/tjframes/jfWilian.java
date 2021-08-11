acced44ed10118b309abd3ef6b0d0b631e5ecfb3
wilian <wilian@DESKTOP-4C5O2JS>
2021-08-11 17:09:53 -0300

    jfWilian

--- src/tjframes/jfWilian.java
+++ src/tjframes/jfWilian.java
@@ -91,7 +91,7 @@ public class jfWilian extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
-     * @param 
+     * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
