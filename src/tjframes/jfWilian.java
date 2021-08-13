b955dbf9f956699216b65b6ed9e1a985188d68ac
wilian <wilian@DESKTOP-4C5O2JS>
2021-08-13 15:37:39 -0300

    jfWilian Calculando idade

--- src/tjframes/jfWilian.java
+++ src/tjframes/jfWilian.java
@@ -6,6 +6,7 @@
 package tjframes;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 import java.text.NumberFormat;
 import javax.swing.JOptionPane;
 
@@ -32,39 +33,29 @@ public class jfWilian extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jtfidade = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jtfResultado = new javax.swing.JTextField();
+        labelResultado = new javax.swing.JLabel();
         jbCalcDias = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        jtfResultado = new javax.swing.JTextField();
+        jtfidade = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jtSoTexto = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jtfidade.setName("jtfIdade"); // NOI18N
-        jtfidade.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jtfidadeActionPerformed(evt);
-            }
-        });
-        jtfidade.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                jtfidadeKeyTyped(evt);
-            }
-        });
+        jPanel1.setBackground(new java.awt.Color(90, 93, 95));
 
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Idade:");
 
-        jLabel2.setText("Resultado : ");
+        labelResultado.setBackground(new java.awt.Color(255, 255, 255));
+        labelResultado.setForeground(new java.awt.Color(255, 255, 255));
+        labelResultado.setText("Resultado: ");
 
-        jtfResultado.setEditable(false);
-        jtfResultado.setName("jtfIdade"); // NOI18N
-        jtfResultado.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jtfResultadoActionPerformed(evt);
-            }
-        });
-
+        jbCalcDias.setBackground(new java.awt.Color(90, 90, 90));
+        jbCalcDias.setForeground(new java.awt.Color(255, 255, 255));
         jbCalcDias.setText("Calcular Dias");
         jbCalcDias.setName("jbCalcDias"); // NOI18N
         jbCalcDias.addActionListener(new java.awt.event.ActionListener() {
@@ -74,28 +65,75 @@ public class jfWilian extends javax.swing.JFrame {
         });
 
         jLabel3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Calcule sua idade");
+
+        jtfResultado.setEditable(false);
+        jtfResultado.setBackground(new java.awt.Color(255, 255, 255));
+        jtfResultado.setName("jtfIdade"); // NOI18N
+        jtfResultado.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtfResultadoActionPerformed(evt);
+            }
+        });
+
+        jtfidade.setBackground(new java.awt.Color(255, 255, 255));
+        jtfidade.setName("jtfIdade"); // NOI18N
+        jtfidade.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtfidadeActionPerformed(evt);
+            }
+        });
+        jtfidade.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jtfidadeKeyPressed(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jtfidadeKeyTyped(evt);
+            }
+        });
+
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Só texto:");
+
+        jtSoTexto.setBackground(new java.awt.Color(255, 255, 255));
+        jtSoTexto.setForeground(new java.awt.Color(0, 0, 0));
+        jtSoTexto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtSoTextoActionPerformed(evt);
+            }
+        });
+        jtSoTexto.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jtSoTextoKeyTyped(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(jtfidade))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(jLabel2)
-                            .addGap(5, 5, 5)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jbCalcDias)
-                                .addComponent(jtfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addComponent(jLabel3))
-                .addContainerGap(71, Short.MAX_VALUE))
+                    .addComponent(jLabel3)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(labelResultado))
+                                .addGap(30, 30, 30)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jbCalcDias, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jtfResultado, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                                .addComponent(jtfidade, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                                .addComponent(jtSoTexto)))))
+                .addGap(0, 102, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -107,43 +145,38 @@ public class jfWilian extends javax.swing.JFrame {
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jtfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelResultado))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jtSoTexto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jbCalcDias)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(61, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(8, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(46, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jtfidadeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtfidadeActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jtfidadeActionPerformed
-
     private void jbCalcDiasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCalcDiasActionPerformed
         // TODO add your handling code here:
 
-        if (jtfidade.getText().equalsIgnoreCase("")) {
+        if (jtfidade.getText().equals("")) {
             jtfResultado.setForeground(Color.red);
             jtfResultado.setText("Idade inválida!");
-            jtfidade.requestFocus();
-            jtfidade.setText("");
+            jtfResultado.setText("");
 
         } else {
             int idade = Integer.parseInt(jtfidade.getText());
@@ -151,13 +184,13 @@ public class jfWilian extends javax.swing.JFrame {
             if (dias < 1) {
                 jtfResultado.setForeground(Color.red);
                 jtfResultado.setText("Idade inválida!");
-                jtfidade.requestFocus();
-                jtfidade.setText("");
+                jtfResultado.setText("");
             } else {
                 jtfResultado.setForeground(Color.BLACK);
                 jtfResultado.setText(NumberFormat.getInstance().format(dias));
                 jtfidade.setText("");
             }
+            jtfResultado.requestFocus();
         }
         //jtfResultado.setText(NumberFormat.getInstance().format(idade*365));
         //JOptionPane.showMessageDialog(this,"Sua idade em dias :"+ (idade *365));
@@ -167,14 +200,39 @@ public class jfWilian extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jtfResultadoActionPerformed
 
+    private void jtfidadeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtfidadeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtfidadeActionPerformed
+
+    private void jtfidadeKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfidadeKeyPressed
+        // TODO add your handling code here:
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+
+            this.jbCalcDias.doClick();
+        }
+    }//GEN-LAST:event_jtfidadeKeyPressed
+
     private void jtfidadeKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfidadeKeyTyped
         // TODO add your handling code here:
         String caracteres = "0123456789";
         if (!caracteres.contains(evt.getKeyChar() + "")) {
             evt.consume();
-
+            JOptionPane.showMessageDialog(rootPane, "Somente número!");
         }
     }//GEN-LAST:event_jtfidadeKeyTyped
+
+    private void jtSoTextoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtSoTextoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtSoTextoActionPerformed
+
+    private void jtSoTextoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtSoTextoKeyTyped
+        // TODO add your handling code here:
+        String caracteres = "0123456789";
+        if (caracteres.contains(evt.getKeyChar() + "")) {
+            evt.consume();
+            JOptionPane.showMessageDialog(rootPane, "Somente Caractere!");
+        }
+    }//GEN-LAST:event_jtSoTextoKeyTyped
 
     /**
      * @param args the command line arguments
@@ -217,7 +275,9 @@ public class jfWilian extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbCalcDias;
+    private javax.swing.JTextField jtSoTexto;
     private javax.swing.JTextField jtfResultado;
     private javax.swing.JTextField jtfidade;
+    private javax.swing.JLabel labelResultado;
     // End of variables declaration//GEN-END:variables
 }
