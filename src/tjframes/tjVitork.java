45052f9b93c62535a09568ffd630ca480171936e
admin <admin@nbmpc058>
2021-08-14 09:49:32 -0300

    tjFrames senac

--- src/tjframes/tjVitork.java
+++ src/tjframes/tjVitork.java
@@ -6,6 +6,7 @@
 package tjframes;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 import java.text.NumberFormat;
 import javax.swing.JOptionPane;
 
@@ -34,18 +35,32 @@ public class tjVitork extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         JbfIdade = new javax.swing.JTextField();
         jLabell = new javax.swing.JLabel();
-        CalcDias = new javax.swing.JButton();
+        jtfCalcDias = new javax.swing.JButton();
         jbfResultado = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jtfValor = new javax.swing.JTextField();
+        jrdbKm2m = new javax.swing.JRadioButton();
+        jrdbM2km = new javax.swing.JRadioButton();
+        jtfConverter = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        JbfIdade.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                JbfIdadeKeyPressed(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                JbfIdadeKeyTyped(evt);
+            }
+        });
+
         jLabell.setText("idade:");
 
-        CalcDias.setText("Calcular Dias");
-        CalcDias.addActionListener(new java.awt.event.ActionListener() {
+        jtfCalcDias.setText("Calcular Dias");
+        jtfCalcDias.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CalcDiasActionPerformed(evt);
+                jtfCalcDiasActionPerformed(evt);
             }
         });
 
@@ -53,38 +68,68 @@ public class tjVitork extends javax.swing.JFrame {
 
         jLabel1.setText("Resultado:");
 
+        jLabel2.setText("Valor");
+
+        jrdbKm2m.setText("de Km para Milhas");
+
+        jrdbM2km.setText("de Milhas para km");
+
+        jtfConverter.setText("Converter");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(CalcDias)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jtfCalcDias)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabell, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jbfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(JbfIdade, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabell, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jbfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(JbfIdade, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(131, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jrdbKm2m, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jrdbM2km, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jtfConverter, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(13, 13, 13))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addGap(15, 15, 15)
+                        .addComponent(jtfValor)))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
+                .addGap(6, 6, 6)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jtfValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(JbfIdade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jrdbKm2m)
                     .addComponent(jLabell))
                 .addGap(4, 4, 4)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jrdbM2km)
                     .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(CalcDias)
-                .addContainerGap(181, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jtfCalcDias)
+                    .addComponent(jtfConverter))
+                .addContainerGap(178, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -95,18 +140,18 @@ public class tjVitork extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+  
 
-    private void CalcDiasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CalcDiasActionPerformed
+    private void jtfCalcDiasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtfCalcDiasActionPerformed
         // TODO add your handling code here:
         //jbfResultado.setText(Integer.toString(idade*365));
-        if(JbfIdade.getText().equalsIgnoreCase("")){
+        
+        if(JbfIdade.getText().equals("")){
             jbfResultado.setForeground(Color.red);
             jbfResultado.setText("Não tem valor");
             JbfIdade.requestFocus();
@@ -120,17 +165,57 @@ public class tjVitork extends javax.swing.JFrame {
                 jbfResultado.setForeground(Color.red);
             }
             
+    
+        //jbfResultado.setText(NumberFormat.getInstance().format(idade*365));
+        //JOptionPane.showMessageDialog(null,"Sua idade em dias e: "+(idade*365));
         
-        jbfResultado.setText(NumberFormat.getInstance().format(idade*365));
-        JOptionPane.showMessageDialog(null,"Sua idade em dias e: "+(idade*365));
-        JbfIdade.setText("");
-    }//GEN-LAST:event_CalcDiasActionPerformed
+        JbfIdade.requestFocus();
+    }//GEN-LAST:event_jtfCalcDiasActionPerformed
     }
+    private void JbfIdadeKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_JbfIdadeKeyTyped
+    String caracteres="0123456789";
+    if(!caracteres.contains(evt.getKeyChar()+"")){
+    evt.consume();
+     JOptionPane.showMessageDialog(this,"Somente numeros");
+            }// TODO add your handling code here:
+    }//GEN-LAST:event_JbfIdadeKeyTyped
+
+
+private void jtfSomTextoKeyTyped(java.awt.event.KeyEvent evt){
+String caracteres="0123456789";
+    if(!caracteres.contains(evt.getKeyChar()+"")){
+    evt.consume();
+  }
+}
+    private void JbfIdadeKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_JbfIdadeKeyPressed
+       if(evt.getKeyCode()== KeyEvent.VK_ENTER){
+           this.jtfCalcDias.doClick();
+    }//GEN-LAST:event_JbfIdadeKeyPressed
+}
+    private void jbConverterActionPerformed(java.awt.event.ActionEvent evt){
+     if(!jtfValor.getText().equals("")){
+       double result=0;
+      double valor = Double.perseDouble(jtfValor.getText());
+      
+      if(jrdbKm2m.isSelected()){
+          result= valor/ 1.6;
+      }else if (jrdbM2km.isSelected()){
+         result = valor*1.6; 
+      }  
+      JOptionPane.showMessageDialog(this,"O resultado e: "+ result ,
+              NumberFormat.getInstance().format(result),
+              "Conversão",JOptionPane.INFORMATION_MESSAGE);
+  }else{
+   JOptionPane.showMessageDialog(this,"Valor nao pode ser vazio");
+   jtfValor.requestFocus();
+}
+}
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        
+ 
         
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -159,16 +244,22 @@ public class tjVitork extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new tjVitork().setVisible(true);
-            }
+}   
         });
-    }
 
+}
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton CalcDias;
     private javax.swing.JTextField JbfIdade;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabell;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jbfResultado;
+    private javax.swing.JRadioButton jrdbKm2m;
+    private javax.swing.JRadioButton jrdbM2km;
+    private javax.swing.JButton jtfCalcDias;
+    private javax.swing.JButton jtfConverter;
+    private javax.swing.JTextField jtfValor;
     // End of variables declaration//GEN-END:variables
 }
+
